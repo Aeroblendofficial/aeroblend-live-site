30e0a0554f8fa887a6fddfb0ddded4db7eeb053a
Aeroblendofficial <jkphotos302@gmail.com>
2025-10-28 16:14:24 +0530

    Create Preorderform.js

---
+++ components/Preorderform.js
@@ -0,0 +1,12 @@
+export default function PreorderForm() {
+  return (
+    <div style={{
+      textAlign: 'center',
+      padding: '2rem',
+      color: '#fff'
+    }}>
+      <h2>Pre-order your AeroBlend bottle soon!</h2>
+      <p>Stay hydrated, stay futuristic 🌿</p>
+    </div>
+  );
+}
