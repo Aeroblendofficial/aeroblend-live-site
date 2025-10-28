c4034210197d161159fe2b8b21c5dc704b8cfb7a
Aeroblendofficial <jkphotos302@gmail.com>
2025-10-28 16:11:57 +0530

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header style={{
+      textAlign: 'center',
+      padding: '1rem',
+      fontSize: '1.5rem',
+      fontWeight: 'bold',
+      color: '#00f0c8'
+    }}>
+      AeroBlend 💧
+    </header>
+  );
+}
